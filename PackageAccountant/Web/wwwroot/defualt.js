e8df1620dfe7ed96f2b33d915f0a55b605e9eb26
JoeHyuga <tongtng661@qq.com>
2019-08-18 15:29:34 +0800

    front end use angularjs

---
+++ PackageAccountant/Web/wwwroot/defualt.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module('PackageApp', ['ngRoute']);
+
+app.controller("UplaodController", function ($scope, $http) {
+    $scope.Upload = function () {
+        var form = new FormData();
+        var file = $("#file")[0].files[0];
+        form.append("fileName", file);
+        $http.post("/api/values/UploadFiles", form, {
+            transformRequest: angular.identity,
+            headers: {
+                'Content-Type': undefined
+            }
+        }).then(function (data) {
+            alert("true");
+            }).catch(function (data) {
+                console.log(data);
+            });
+        //$.post('/api/values/UploadFiles',
+        //    {
+        //        data: form,
+        //        transformRequest: angular.identity,
+        //        headers: {
+        //            'Content-Type': undefined
+        //        }
+        //    }, function (data) {
+        //        console.log(data);
+        //    });
+    }
+})
