cb518cb2e928615c2add4c74b17ae3ec4312a378
JoeHyuga <tongtng661@qq.com>
2019-11-19 09:00:07 +0800

    ttt

--- PackageAccountant/BLL/Operate/AccountItermDetailsBll.cs
+++ PackageAccountant/BLL/Operate/AccountItermDetailsBll.cs
@@ -38,7 +38,6 @@ namespace BLL.Operate
                     };
                     unit.AccountItermDetailsRepository.Insert(accountIterm);
                 }
-
             }
             unit.saveChange();
         }
