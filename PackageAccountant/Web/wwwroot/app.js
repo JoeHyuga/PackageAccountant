f3419d03a70a1a81bf6920ab97021ea2c7ff752b
JoeHyuga <tongtng661@qq.com>
2020-02-03 15:03:44 +0800

    add chart js

--- PackageAccountant/Web/wwwroot/app.js
+++ PackageAccountant/Web/wwwroot/app.js
@@ -1,20 +1,20 @@
 ﻿var app = angular.module('PackageApp', ['ngRoute']);
 
 app.controller('UplaodController', function ($scope, $route) { $scope.$route = $route; })
-app.controller('LoginController', function ($scope, $route) { $scope.$route = $route; })
+app.controller('InExShowController', function ($scope, $route) { $scope.$route = $route; })
 app.config(['$locationProvider', function ($locationProvider) {
     $locationProvider.hashPrefix('');
 }]);
 
 app.config(function ($routeProvider) {
     $routeProvider.
-        when('/login', {
-            templateUrl: 'login.html',
-            controller: 'LoginController'
-        }).
         when('/default', {
             templateUrl: 'default.html',
             controller: 'UplaodController'
+        }).
+        when('/inexshow', {
+            templateUrl: 'inexshow.html',
+            controller: 'InExShowController'
         }).
         otherwise({
             redirectTo: '/default'
---
+++ PackageAccountant/Web/wwwroot/inexshow.js
@@ -0,0 +1,17 @@
+﻿app.controller("InExShowController", function ($scope, $http) {
+    $scope.$emit('page', "收支总览");
+    var ctx = $("#myChart")
+    var Cx = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }]
+        },
+        options: {}
+    })
+})
